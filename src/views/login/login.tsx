dbed9addc2c0b313ab4fad70154e160969165c7f
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-01-03 11:16:48 -0300

    se agrega link a login y creacion del componente

---
+++ src/views/login/login.tsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom'
+import './login.css'
+
+export const Login = () => {
+    return(
+        <>
+        <NavLink to='/home'>Volver</NavLink>
+        <p>Login</p>
+        </>
+    )
+}
